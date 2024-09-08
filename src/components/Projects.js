ac92b530840f7651ac1c394a20eab76fcaa9d73f
Mahir Rahman <mahir199.tanzil@gmail.com>
2024-09-08 18:04:33 -0400

    2.5

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,10 @@
-import { Col, Row } from "react-bootstrap"
+import { Col, Container, Tab, Row, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCards";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+
 
 export const Projects = () => {
 
@@ -6,32 +12,32 @@ export const Projects = () => {
         {
             title: "Business Startup",
             description: "Design & Development",
-            imgUrl: progImg1,
+            imgUrl: projImg1,
         },
         {
             title: "Business Startup",
             description: "Design & Development",
-            imgUrl: progImg2,
+            imgUrl: projImg2,
         },
         {
             title: "Business Startup",
             description: "Design & Development",
-            imgUrl: progImg3,
+            imgUrl: projImg3,
         },
         {
             title: "Business Startup",
             description: "Design & Development",
-            imgUrl: progImg1,
+            imgUrl: projImg1,
         },
         {
             title: "Business Startup",
             description: "Design & Development",
-            imgUrl: progImg2,
+            imgUrl: projImg2,
         },
         {
             title: "Business Startup",
             description: "Design & Development",
-            imgUrl: progImg3,
+            imgUrl: projImg3,
         },
     ]
     return (
@@ -41,9 +47,43 @@ export const Projects = () => {
                     <Col>
                         <h2>Projects</h2>
                         <p>Hi my name mahir and this about projects</p>
+                        <Tab.Container id= "projects-tab" defaultActiveKey="first">
+                            <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                                <Nav.Item>
+                                    <Nav.Link eventKey="first">Tab One</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="third">
+                                        Tab Three
+                                    </Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                            <Tab.Content>
+                                <Tab.Pane eventKey="first">
+                                    <Row>
+                                        {
+                                            projects.map((projects, index) => {
+                                                return (
+                                                    <ProjectCard
+                                                    key={index}
+                                                    {...projects}
+                                                    />
+                                                )
+                                            })
+                                        }
+                                    </Row>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="second">Mahir</Tab.Pane>
+                                <Tab.Pane eventKey="third">Rahman</Tab.Pane>
+                            </Tab.Content>
+                        </Tab.Container>
                     </Col>
                 </Row>
             </Container>
+            <img className="background-image-right" src={colorSharp2}></img>
         </section>
     )
 }
